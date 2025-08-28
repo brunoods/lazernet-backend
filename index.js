1d5ed3afb3ebaaa74f9cc66fc2830f17f4f3538d
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-08-28 15:57:51 -0300

    Fix Version IA

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const chatbotSchema = z.object({
 
 // INSTÂNCIAS DE SERVIÇOS
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT, 10),
