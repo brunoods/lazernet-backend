fe44b241b67e33881bc692b2f69787402d8aa8c5
brunooliveiradsv <brunooliveiradsv@gmail.com>
2025-08-28 15:21:29 -0300

    Update IA

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const chatbotSchema = z.object({
 
 // --- INSTÂNCIAS DE SERVIÇOS ---
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
 // 4. TRANSPORTER DO NODEMAILER CENTRALIZADO
 const transporter = nodemailer.createTransport({
